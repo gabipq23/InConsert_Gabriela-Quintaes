ee47a73937289ca2abe9e5700f84ccce7b1f4c29
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2024-04-29 15:10:52 -0300

    adição de dados com firebase

--- src/componentes/Cards/Card.jsx
+++ src/componentes/Cards/Card.jsx
@@ -1,35 +1,25 @@
-import { Component } from 'react';
 import styles from './Card.module.css'
 
 import { FaRegHeart } from "react-icons/fa";
 import { FaRegCommentDots } from "react-icons/fa";
 
-export class Card extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            titulo:props.titulo,
-            descricao:props.descricao,
-            artista:props.artista,
-            local:props.local
+export function Card(props) {
+  const post = props.post;
 
-        }
-    }
-    render() {
-      return (
-        
-        <div className={styles["containerPosts"]}>
-          <h3>{this.state.titulo}</h3>
-          <p>Artista/Banda: {this.state.artista}</p>
-          <p>Local: {this.state.local}</p>
-          <p>Descrição: {this.state.descricao}</p>
-
-          <div className={styles["icons"]}> 
-            <span className={styles["iconItem"]}><FaRegHeart size='20px'/></span>
+  return (
+    
+    <div className={styles["containerPosts"]}>
+        <p>{post.titulo}</p>
+        <p>{post.artista}</p>
+        <p>{post.local}</p>
+        <p>{post.descricao}</p>
+   
+        <div className={styles["icons"]}> 
+             <span className={styles["iconItem"]}><FaRegHeart size='20px'/></span>
             <span className={styles["iconItem"]}><FaRegCommentDots size='20px' /></span>
-          </div>
+         </div>
 
-        </div>
-      )
-    }
+      </div>
+    
+  )
 }
--- src/componentes/PostList/PostList.jsx
+++ src/componentes/PostList/PostList.jsx
@@ -1,23 +1,51 @@
 import {Card} from '../Cards/Card'
 import styles from './PostList.module.css'
-
-export function PostList({ handlePlus }){
-    return(
-        <div className={styles["container"]}>
-            
-            <div  className={styles["containerPosts"]}> 
-            <Card titulo={'Post #1'} artista={'Justin Bieber'} local={'Rock in Rio'} descricao={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
-            <Card titulo={'Post #2'} artista={'Taylor Swift'} local={'Engenhão - RJ'} descricao={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
-            <Card titulo={'Post #3'} artista={'The Weeknd'}  local={'Engenhão - RJ'} descricao={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}/>
-
-            <div className={styles["buttonContainer"]}>
-                <button onClick={handlePlus} className={styles["buttonPlus"]}>+</button>
-            </div>
-
-            </div>
-
-        </div>
-    )
-}
+import { useEffect } from "react";
+import { useState } from "react";
 
 // https://www.figma.com/file/x44d6b3o5vxIOJFQZiPZBO/Untitled?type=design&node-id=0-1&mode=design&t=PwDwl0A7y8KMTQW1-0
+
+
+export function PostList({ handlePlus }) {
+
+    const [posts, setPosts] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    const baseUrl = 'https://inconsert-pb-13d11-default-rtdb.asia-southeast1.firebasedatabase.app/'
+
+    function convertData(data) {
+      const ids = Object.keys(data);
+      let posts = Object.values(data);
+      return posts.map((post,index) =>{
+        return{
+          id:ids[index], ...post,
+        };
+      });
+    }
+
+    useEffect(() => {
+      fetch(`${baseUrl}/postList.json`).then(async (resp) => {
+        const respPosts = await resp.json();
+        let convertedPosts = convertData(respPosts);
+        setPosts(convertedPosts);
+      }).catch((erro) => setMessage(erro.meesage)).finally(() => setLoading(false));
+    },[]);
+    
+  return (
+    <div className={styles["container"]}>
+            
+             <div  className={styles["containerPosts"]}> 
+      {isLoading && <p>Carregando...</p>}
+      {message && <p>Erro: {message}</p>}
+      {posts && posts.map((post) => <Card key={post.id} post={post} />)}
+      
+      </div>
+      <div className={styles["buttonContainer"]}>
+               <button onClick={handlePlus} className={styles["buttonPlus"]}>+</button>
+    </div>
+
+    
+    </div>
+  )
+}
